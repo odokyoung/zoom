06ef0ee71b73f06da19da89c2592f8d724bb0597
odogyeong <echo.odk@gmail.com>
2025-04-14 22:35:04 +0900

    FEAT: first commit

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import express, {application} from 'express';
 
 const app = express();
+const port = 3000;
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/public/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+	res.render("home");
+});
 
-app.listen(3000);
+const handleListening = () => console.log(`Listening on http://localhost:${port}`);
+app.listen(port, handleListening);
